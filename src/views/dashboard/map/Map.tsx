6265a41e36202a0026b797b4db62a2a07e9a9752
santisans123 <santitok21@gmail.com>
2023-09-01 10:40:35 +0700

    create profile

--- src/views/dashboard/map/Map.tsx
+++ src/views/dashboard/map/Map.tsx
@@ -2,15 +2,20 @@ import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 function LeafletMap() {
 	return (
-		<MapContainer
-			
-		>
-			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			<Marker position={[51.505, -0.09]}></Marker>
-		</MapContainer>
+		<div>
+			<h1>Leaflet Map Example</h1>
+			<div style={{ height: '400px', width: '100%' }}>
+				<MapContainer center={[51.505, -0.09]} zoom={13}>
+					<TileLayer
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					/>
+					<Marker position={[51.505, -0.09]}>
+						{/* You can add any additional components or popups here */}
+					</Marker>
+				</MapContainer>
+			</div>
+		</div>
 	);
 }
 
