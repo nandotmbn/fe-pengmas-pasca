6f66cc10296eb2192c31645c88f1a22cdd8c0fc8
masmuss <ahmusafir.khoirul@gmail.com>
2023-09-22 15:18:21 +0700

    fix: map size for mobile
    Signed-off-by: masmuss <ahmusafir.khoirul@gmail.com>

--- src/views/home/partials/Footer.tsx
+++ src/views/home/partials/Footer.tsx
@@ -28,8 +28,8 @@ export default function Footer() {
 						<h5 className="mb-2 mt-0 text-lg text-gray-700">
 							Ruang PS 4.17. Politeknik Elektronika Negeri Surabaya
 						</h5>
-						<div className="mt-6">
-						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15830.737602343857!2d112.78725204769763!3d-7.276706145621895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa10ea2ae883%3A0xbe22c55d60ef09c7!2sPoliteknik%20Elektronika%20Negeri%20Surabaya!5e0!3m2!1sen!2sid!4v1692591292899!5m2!1sen!2sid" width="500" height="150" loading="lazy" ></iframe>
+						<div className="mt-6 overflow-hidden">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15830.737602343857!2d112.78725204769763!3d-7.276706145621895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa10ea2ae883%3A0xbe22c55d60ef09c7!2sPoliteknik%20Elektronika%20Negeri%20Surabaya!5e0!3m2!1sen!2sid!4v1692591292899!5m2!1sen!2sid" width="500" height="150" loading="lazy" className="aspect-video"></iframe>
 						</div>
 					</div>
 					<div className="w-full px-4 lg:w-6/12">
